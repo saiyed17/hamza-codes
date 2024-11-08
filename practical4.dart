879b4ca5265abe3f1395f8766fb52f3d613fa94e
saiyed17 <167959128+saiyed17@users.noreply.github.com>
2024-11-08 15:07:53 +0530

    Add files via upload

---
+++ practical4.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Working with Images'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Replace network image with local image
+              Image.asset(
+                'assets/bike.jpeg', // Update with your image path
+                width: 400,
+                height: 200,
+              ),
+              Text(
+                'Welcome to NUV',
+                style: TextStyle(fontSize: 50.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
